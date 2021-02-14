6ccffbd6a71fea97245014945da469c2bd301fef
VHM CODER <58830759+VHMCODERhoff@users.noreply.github.com>
2021-02-14 20:02:29 -0300

    Add files via upload

---
+++ Descricao_Description.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+      JOptionPane.showMessageDialog(null,"Essa eh uma lista em java com 9 exercicios, This is a list in java with 9 exercises!", "Lista", JOptionPane.INFORMATION_MESSAGE);          
+       
+    }
+}

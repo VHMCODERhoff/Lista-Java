093305544385a3ae28c44e8b9ce5d57f66c13b4c
VHM CODER <58830759+VHMCODERhoff@users.noreply.github.com>
2021-02-20 00:09:42 +0000

    Update calculo.java

--- CalculoNumeroInteiroIF - Ex6/src/calculo/calculo.java
+++ CalculoNumeroInteiroIF - Ex6/src/calculo/calculo.java
@@ -6,7 +6,8 @@ public class calculo {
     public static void main(String[] args){
         int Leia = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira Um Numero Inteiro:","Comando If",JOptionPane.INFORMATION_MESSAGE));
 
-        if (Leia > 0 || Leia == 0){
+        if (Leia < 0 ){
+            Leia = (-1) * Leia;
             JOptionPane.showMessageDialog(null,"O Numero Digitado eh Igual a 0 ou Maior a 0", "Comando If", JOptionPane.INFORMATION_MESSAGE);
 
         }else {
